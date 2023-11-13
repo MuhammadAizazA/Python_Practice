be44be6b8b8794134463b3abc16614c27a2ba243
MuhammadAizazA <aizaz.ali.shah.0205@gmail.com>
2023-11-13 10:30:58 +0500

    working with the objects of circle class

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from circle import Circle
 
 first_circle_object=Circle(10)
 z=first_circle_object.calculate_area()
-print(z)
+print(first_circle_object.radius,' ',z)
 second_circle_object=Circle(27)
 x=second_circle_object.calculate_area()
-print(x)
+print(second_circle_object.radius,' ',x)
