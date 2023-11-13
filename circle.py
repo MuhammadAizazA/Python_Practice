66ae00075dba427dc86d4311431b856c4b5864ab
MuhammadAizazA <aizaz.ali.shah.0205@gmail.com>
2023-11-13 10:26:31 +0500

    There was indentation issue in calculate_area fun

--- circle.py
+++ circle.py
@@ -4,5 +4,5 @@ class Circle:
     def __init__(self,radius):
         self.radius=radius
 
-def calculate_area(self):
-    return round(math.pi * self.radius**2,2)
+    def calculate_area(self):
+        return round(math.pi * self.radius**2,2)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+from circle import Circle
+
+first_circle_object=Circle(10)
+
+second_circle_object=Circle(27)
