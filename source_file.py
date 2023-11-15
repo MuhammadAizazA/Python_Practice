85dd7541054641cab519256901666de6ec7eb9c7
MuhammadAizazA <aizaz.ali.shah.0205@gmail.com>
2023-11-15 12:47:14 +0500

    Modifications

--- source_file.py
+++ source_file.py
@@ -1,9 +1,10 @@
+import sys as s
 new_path = {"key": "C:\\Users\\aizaz\\OneDrive\\Desktop"}
 s.path.append(new_path['key'])
 
 from module import print_something as ps, name as nm
-import sys as s
 import sample as sam
+
 
 
 
@@ -15,3 +16,5 @@ print(sam.first_dict)
 
 print(s.path,"\n\n")
 print("Path of sample file is: ",sam.__file__,"\n\n")
+
+print(dir(sam))
