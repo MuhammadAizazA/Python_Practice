88f5d080dcfafa0c1f74df478e187fb736c248f4
MuhammadAizazA <aizaz.ali.shah.0205@gmail.com>
2023-11-15 11:22:47 +0500

    new code inserted

--- module.py
+++ module.py
@@ -1,6 +1,8 @@
 def print_something():
     print("Pakistan")
     
-if __name__=='__main__':
-    print_something()
-    print(f'{__name__} is opened')
+name="aizaz"
+
+
+print_something()
+print(f'{__name__} is opened')
--- source_file.py
+++ source_file.py
@@ -1,4 +1,17 @@
-from module import print_something
+new_path = {"key": "C:\\Users\\aizaz\\OneDrive\\Desktop"}
+s.path.append(new_path['key'])
 
-print_something()
+from module import print_something as ps, name as nm
+import sys as s
+import sample as sam
+
+
+
+ps()
+print(nm)
 print("User File")
+
+print(sam.first_dict)
+
+print(s.path,"\n\n")
+print("Path of sample file is: ",sam.__file__,"\n\n")
